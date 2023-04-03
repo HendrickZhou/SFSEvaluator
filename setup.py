1047772bfe3ecc0c95cdd02345472127a5bf2342
Hang Zhou <zhou_hang@berkeley.edu>
2023-04-03 12:13:05 -0400

    add req

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 import os
 lib_folder = os.path.dirname(os.path.realpath(__file__))
 requirement_path = lib_folder + '/requirements.txt'
-install_requires = []
-
+with open(requirement_path) as f:
+    install_requires = list(f.read().splitlines())
 setup(
     name='sfseval',
     install_requires=install_requires,
