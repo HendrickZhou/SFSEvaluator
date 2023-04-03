3f0607044664b5bb1553e3e2066d6109b12fb5a5
Hang Zhou <zhou_hang@berkeley.edu>
2023-04-03 12:18:47 -0400

    don

--- setup.py
+++ setup.py
@@ -1,11 +1,5 @@
 from setuptools import setup
 
-import os
-lib_folder = os.path.dirname(os.path.realpath(__file__))
-requirement_path = lib_folder + '/requirements.txt'
-with open(requirement_path) as f:
-    install_requires = list(f.read().splitlines())
-
 install_requires=[
     'llist==0.7.1',
     'matlabengineforpython==9.13',
